f465b165eea49b95cdbcca13f719b5ffef444bb4
Igor <igorz@cybcube.com>
2022-04-29 13:20:57 +0300

    Updates to work with patterns via graphql api

--- src/action.js
+++ src/action.js
@@ -1,5 +1,6 @@
 const core = require('@actions/core');
 const { request } = require("@octokit/request");
+const { graphql } = require("@octokit/graphql");
 const fs = require('fs')
 
 var excludedReposPath = '';
@@ -9,212 +10,217 @@ async function run() {
     const token = core.getInput("token");
     const orgName = core.getInput("org")
     const rulesPath = core.getInput("rulesPath");
+    const action = core.getInput("action");
+
     excludedReposPath = core.getInput("excludedReposPath");
     includedReposPath = core.getInput("includedReposPath");
-    const action = core.getInput("action");
-    const limit = 100;
+
     var rulesObj;
     var branches;
+
     try {
-        if(!fs.existsSync(rulesPath)){
-            throw "Rules JSON is missing."
+        if (!fs.existsSync(rulesPath)) {
+            throw "Specified branch protection configuration file is missing: " + rulesPath;
         }
-        const rules = fs.readFileSync(rulesPath, {encoding:'utf8', flag:'r'});
+
+        const rules = fs.readFileSync(rulesPath, { encoding: 'utf8', flag: 'r' });
         rulesObj = JSON.parse(rules);
         keys = Object.keys(rulesObj);
-        var repos = await getFinalRepos(token, orgName);  
-        for (let i = 0; i < repos.length; i++) {
-            branches = await getBranches(token, repos[i], keys);  
-            for (let j = 0; j < branches.length; j++) {
-                if(branches[j].protected){
-                    console.log("Deleting Branch Protection for " + branches[j].name + " branch of " + repos[i]);
-                    core.debug("Deleting Branch Protection for " + branches[j].name + " branch of " + repos[i]);
-                    await deleteProtection(token, repos[i], branches[j].name);
+        var filtered_repos = await getFinalRepos(token, orgName);
+
+
+        for (let [repo_name, repo_id] of filtered_repos) {
+            protectionRuleIds = await getBranchesProtectionIds(token, orgName, repo_name);
+
+            console.log("Deleting Branch Protection for repo " + repo_name);
+            getBranchesProtectionIds(token, orgName, repo_name).forEach(async (protectionRuleId) => {
+                await deleteBranchesProtection(token, protectionRuleId)
+            });
+
+            if (action == "set") {
+                console.log("Setting Branch Protection for " + branches[j].name + " branch of " + repo_name);
+
+                try {
+                    await createBranchProtection(token, repo_id, rulesObj[branches[j].name]);
+                } catch (error) {
+                    console.error("Branch protection rule creation request failed for repo: " + repo_name, error.message);
                 }
-                if(action == "set"){
-                    console.log("Setting Branch Protection for " + branches[j].name + " branch of " + repos[i]);
-                    core.debug("Setting Branch Protection for " + branches[j].name + " branch of " + repos[i]);
-                    await setProtection(token, repos[i], branches[j].name, rulesObj[branches[j].name] )
-                }
-            }     
-        }
-  }
-  catch(e){
-    console.error(e);
-    core.setFailed(e.stack);
-  }
-}
-
-async function setProtection(token, repoName, branchName, ruleData){
-    const url = "/repos/" + repoName + "/branches/" + branchName + "/protection"
-    if(ruleData == ""){
-        ruleData = {
-            'required_status_checks': None,
-            'restrictions': {
-                'users': [],
-                'teams': [],
-            },
-        }
-    }
-    try {
-        const result = await request("PUT " + url, {
-            headers: {
-            authorization: "token " + token,
-            },
-            data: ruleData
-        });
-        //console.log(result.data);
-        core.debug(result.data);
-    }
-    catch(e){
-        console.error(e);
-        core.setFailed("Exception Occurred in Set Protection: " + e.stack);
-    }
-}
-
-async function deleteProtection(token, repoName, branchName){
-    const url = "/repos/" + repoName + "/branches/" + branchName + "/protection"
-    try{
-        const result = await request("DELETE " + url, {
-            headers: {
-            authorization: "token " + token,
-            }
-        });
-        if(result.status != 204){
-            throw "Exception occured during Delete Protection";
-        }
-    }
-    catch(e){
-        console.error(e);
-        core.setFailed("Exception Occurred in Delete Protection: " + e.stack);
-    }
-}
-
-async function getBranches(token, repoName, branchNames){
-    branchInfoArr = [];
-    const url = "/repos/" + repoName + "/branches"
-    try {
-        const result = await request("GET " + url, {
-            headers: {
-            authorization: "token " + token,
-            }
-        });
-        branchData = result.data;
-        for (let j = 0; j < branchData.length; j++) {
-            const element = branchData[j];
-            if(branchNames.includes(element.name)){
-                branchInfoArr.push(element);
             }
         }
     }
-    catch(e) {
-        console.error(e);
-        core.setFailed("Exception Occurred in Get Branches: " + e.stack);
+    catch (e) {
+        core.setFailed(e.stack);
     }
-    return branchInfoArr;
 }
 
-async function getRepoCount(token, orgName){
+async function createBranchProtection(token, repoId, protectionConfig) {
+
+    protectionConfig["repositoryId"] = repoId;
+
+    await graphql({
+        query: `mutation createBranchProtection($input: CreateBranchProtectionRuleInput!)  {
+            createBranchProtectionRule(input: $input)
+            {
+                clientMutationId
+            }
+        }`,
+        repo: repoId,
+        input: protectionConfig,
+        headers: {
+            authorization: "token " + token,
+        },
+    });
+
+}
+
+async function deleteBranchesProtection(token, ruleId) {
+    await graphql({
+        query: `mutation deleteBranchProtection($branchProtectionRuleId: ID!)  {
+            deleteBranchProtectionRule(input: {
+                branchProtectionRuleId: $branchProtectionRuleId
+            })
+            {
+                clientMutationId
+            }
+        }`,
+        branchProtectionRuleId: ruleId,
+        headers: {
+            authorization: "token " + token,
+        },
+    });
+}
+
+async function getBranchesProtectionIds(token, org, repoName) {
+
+    const { repository: { branchProtectionRules: { nodes: id } } } = await graphql({
+        query: `query protectedBranches($owner: String!, $repo: String!) {
+          repository(owner:$owner, name:$repo) {
+            branchProtectionRules(first: 100) {
+              nodes {
+                id
+              }
+            }
+          }
+        }`,
+        owner: org,
+        repo: repoName,
+        headers: {
+            authorization: "token " + token,
+        },
+    });
+
+
+    return id.map(p => p.id)
+}
+
+async function getRepoCount(token, orgName) {
     repoCnt = 0;
     try {
         const result = await request("GET /orgs/{org}/repos", {
             headers: {
-            authorization: "token " + token,
+                authorization: "token " + token,
             },
             org: orgName,
-            per_page:1,
+            per_page: 1,
             type: "all"
         });
-        const respUrl = new URL(result.headers.link.split(',')[1].split(';')[0].trim().replace('<','').replace('>',''));
+        const respUrl = new URL(result.headers.link.split(',')[1].split(';')[0].trim().replace('<', '').replace('>', ''));
         repoCnt = parseInt(respUrl.searchParams.get('page'));
     }
-    catch(e){
-        console.error(e);
+    catch (e) {
         core.setFailed("Exception Occurred in Get Repo Count: " + e.stack);
     }
     return repoCnt;
 }
 
-function getPageCount(itemCount, limit){
-    pageCount = (itemCount < limit) ? 1 : (((itemCount % limit) > 0) ? (Math.floor(itemCount/limit) + 1) : (itemCount/limit));
+function getPageCount(itemCount, limit) {
+    pageCount = (itemCount < limit) ? 1 : (((itemCount % limit) > 0) ? (Math.floor(itemCount / limit) + 1) : (itemCount / limit));
     return pageCount;
 }
 
-async function getPagedRepos(token, orgName, pageNum, limit){
-    var repos = [];
+async function getAllRepos(token, orgName) {
+    all_repos = new Map();
+    limit = 100;
+
+    repoCount = await getRepoCount(token, orgName);
+    pageCnt = getPageCount(repoCount, limit);
+
+    for (let i = 0; i < pageCnt; i++) {
+        i = i + 1;
+        pagedRepos = await getPagedRepos(token, orgName, i, limit);
+
+        for (let [repo_name, repo_id] of pagedRepos) {
+            repoShortName = repo_name.replace(orgName + "/", "");
+            all_repos.set(repoShortName, repo_id);
+        }
+    }
+
+    return all_repos;
+}
+
+async function getPagedRepos(token, orgName, pageNum, limit) {
+    var repos = new Map();
+
     try {
         const result = await request("GET /orgs/{org}/repos", {
             headers: {
-            authorization: "token " + token,
+                authorization: "token " + token,
             },
             org: orgName,
-            per_page:limit,
+            per_page: limit,
             type: "all",
             page: pageNum
         });
         result.data.forEach(repo => {
-            repos.push(repo.full_name);
+            repos.set(repo.name, repo.node_id);
         });
     }
-    catch(e){
-        console.error(e);
+    catch (e) {
         core.setFailed("Exception Occurred in Get Paged Repos: " + e.stack);
     }
     return repos;
 }
 
-async function getFinalRepos(token, orgName){
-    repos = [];
-    includedRepos = [];
-    limit = 100;
+async function getFinalRepos(token, orgName) {
+
+    var final_repos = new Map();
+    var includedRepos = [];
+
     try {
+        all_repos = await getAllRepos(token, orgName);
         includedRepos = getReposFromFile(includedReposPath);
-        if(includedRepos.length > 0){
-            for (let k = 0; k < includedRepos.length; k++) {
-                includedRepos[k] = orgName + "/" + includedRepos[k];            
-            }
-            return includedRepos;
+
+        if (includedRepos.length > 0) {
+            final_repos = new Map([...all_repos].filter(([k]) => includedRepos.includes(k)));
+            return final_repos;
         }
-        repoCount = await getRepoCount(token, orgName); 
-        pageCnt = getPageCount(repoCount, limit);
+
         excludedRepos = getReposFromFile(excludedReposPath);
-        for (let i = 0; i < pageCnt; i++) {
-            i = i + 1;
-            pagedRepos = await getPagedRepos(token, orgName, i, limit);
-            for (let j = 0; j < pagedRepos.length; j++) {
-                repoShortName = pagedRepos[j].replace(orgName + "/","");
-                if(!excludedRepos.includes(repoShortName)) {
-                    repos.push(pagedRepos[j]);     
-                }      
-            } 
-        }
+
+        final_repos = new Map([...all_repos].filter(([k]) => !excludedRepos.includes(k)));
     }
-    catch(e){
-        console.error(e);
+    catch (e) {
         core.setFailed("Exception Occurred in Get Paged Repos: " + e.stack);
     }
-    return repos;
+    return final_repos;
 }
 
-function getReposFromFile(repoFilePath){
+function getReposFromFile(repoFilePath) {
     repoArr = [];
     try {
-        if(fs.existsSync(repoFilePath)){
-            const reposFrmFile = fs.readFileSync(repoFilePath, {encoding:'utf8', flag:'r'});
-            if(reposFrmFile != ''){
+        if (fs.existsSync(repoFilePath)) {
+            const reposFrmFile = fs.readFileSync(repoFilePath, { encoding: 'utf8', flag: 'r' });
+            if (reposFrmFile != '') {
                 reposFrmFile.trim().split(/\r?\n/).forEach(element => {
-                    if(element != ''){
+                    if (element != '') {
                         repoArr.push(element);
                     }
-                });        
+                });
             }
-        } 
-    }   
-    catch(e){
-        console.error(e);
+        }
+    }
+    catch (e) {
         core.setFailed("Exception Occurred in Get Repos From File: " + e.stack);
     }
     return repoArr;
 }
-
-run();
